fff92d45ce3a04cd458c108346fe3ddd9a8f3b29
Deivison Cardoso <deivisondc@gmail.com>
2021-06-20 16:23:52 -0300

    convert all files into typescript and class components into function components

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,13 +4,20 @@ import {
   useState,
   useCallback,
 } from 'react';
+import { IconType } from 'react-icons';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface IInputProps {
+  name: string;
+  icon?: IconType;
+  placeholder: string;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: IInputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
